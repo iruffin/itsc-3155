3d73b8e1deda11d0ba178788c6119f317614b3a0
Iverson Ruffin <76574672+iruffin@users.noreply.github.com>
2022-09-10 21:07:33 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,8 @@
+# clear out the circle and person txt files
+echo ""
+bash LocalTests/run_person.sh
+echo ""
+bash LocalTests/run_circle.sh
+echo ""
+#rm LocalTests/outputs/circle_test_output.txt
+#rm LocalTests/outputs/person_test_output.txt
